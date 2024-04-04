d7389f8bbab4e1bee5345cecda7ccecbc0e0d782
jrey11 <jared.reyes@coyotes.usd.edu>
2024-04-03 23:21:55 -0500

    Test of Git.

--- febfsk.py
+++ febfsk.py
@@ -95,14 +95,15 @@ letter_to_bits_lowercase = {
     '␒': '10',  # <<STP>> using DC2, does not correspond with Unicode! Will most likely go unused.
     ' ': '00',
 
-    '␂': '0000',  # <<STX>>
-    '␃': '0001',  # <<ETX>>
-    '␄': '0010',  # <<EOT>>
-    '␅': '0011',  # <<ENQ>>
-    '␆': '0100',  # <<ACK>>
-    '␤': '0101',  # <<NWL>>: Will actually print a newline.
-    '␛': '0110',  # <<ESC>>
-    '␀': '0111',  # <<NUL>>: Will actually print nothing. Good for buffers.
+    '␂': '000',  # <<STX>>
+    '␃': '001',  # <<ETX>>
+    '␄': '010',  # <<EOT>>: Will actually end transmission on receiving end.
+    '␅': '011',  # <<ENQ>>
+    '␆': '100',  # <<ACK>>
+    '␤': '101',  # <<NWL>>: Will actually print a newline.
+    '␛': '110',  # <<ESC>>
+    '␀': '111',  # <<NUL>>: Will actually print nothing. Good for buffers.
+
     'e': '1000',
     't': '1001',
     'a': '1010',
